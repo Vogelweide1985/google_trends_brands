e95d651c90e575ba42302f173206c2b162518c79
Vogelweide1985 <Vogelweide1985@hotmail.de>
2019-06-19 08:21:19 +0200

    First init

---
+++ functions.r
@@ -0,0 +1,56 @@
+library(gtrendsR)
+library(dplyr)
+
+
+get_one_brand <- function(marke, zeitraum) {
+  if (marke == "") {
+    return(NA)
+  }
+  ls <- gtrends(c(marke), geo = "DE", time = zeitraum)
+  if(is.null(ls[[1]][[1]][[1]])) {
+    print(paste("Keine Daten für", marke, "für den Zeitraum", zeitraum )) 
+    return(NA)
+  }
+  else {
+    print(paste("Lese ein:", marke, ", Zeitraum:", zeitraum))
+    ls
+  }
+}
+
+
+get_all_brands <- function(marken, zeit_chr, zeit_api) {
+marken.ls  <-list()
+  for (m in 1: length(marken)) { 
+    for (z in 1: length(zeit_chr)) {
+      
+      ls <- get_one_brand(marken[m], zeit_api[z]) # Get Trends Data
+      if(is.na(ls)[[1]]) {next}  #Pruefen ob woechentlcihe Daten vorhanden
+      marken.ls[[marken[m]]][[zeit_chr[z]]] <- ls
+    }
+  }
+marken.ls
+}
+
+
+get_interest <- function(gtrends_result, interest = "time") {
+  # options: time, region, city
+  if (interest == "time") {topic <- "interest_over_time"}
+  else if (interest == "region") {topic <- "interest_by_region"}
+  else if (interest == "city") {topic <- "interest_by_city"}
+  else {
+    print("Falsche Auswahl. Bitte wählen zwischen: time, region, city")
+    return(NA)
+  }
+  
+  
+  df <- gtrends_result[[1]][[1]][[topic]]
+  for (m in 1: length(gtrends_result)) {
+    for (z in 1: length(gtrends_result[[m]]))
+      g <- gtrends_result[[m]][[z]][[topic]]
+      g[, "time"] <- unique(gtrends_result[[m]][[z]][[1]]["time"] )
+      df <- bind_rows(df, g)
+  }
+  df
+}
+
+
---
+++ input.r
@@ -0,0 +1,24 @@
+#INIT
+source("functions.r",encoding = "UTF-8")
+config <- config::get()
+
+
+
+# Einlesen aller Individualmarken
+marken   <- unlist(config$category)
+#### Ziehen aller Marken über alle zeitraeume
+ls <- get_all_brands(marken, config[["zeitraum_name"]], config[["zeitraum_api"]])
+#Zeitreihen Dataframe über alle marken und zeitraeume,evtl. fuer bessere Datenvararbeitung
+
+
+df <- get_interest(ls)
+write.csv(df,"hits_by_time.csv", row.names = F)
+df <- get_interest(ls, "region")
+write.csv(df,"hits_by_region.csv", row.names = F)
+df <- get_interest(ls, "city")
+write.csv(df,"hits_by_city.csv", row.names = F)
+
+
+
+names(ls[[2]][[3]][[1]])
+ls[[2]][[2]][[1]]["time"]
